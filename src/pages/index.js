89da8e1d96242ebbd23b3ddd6c41edf929531aca
moosekiss <moosekiss@moosekissui-MacBookPro.local>
2021-10-03 13:24:28 +0900

    Created the pagination component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard date="2021-10-03" title="Test Title" excerpt="excerpt test" slug="slug test" />
+      </Content>
     </Container>
   )
 }
